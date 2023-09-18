7f14503aa52b84916b0cfc18e3efc60ffc053e43
Brian Bandi <briankim7587@gmail.com>
2023-09-18 19:00:20 +0300

    second commit

--- src/main.jsx
+++ src/main.jsx
@@ -14,7 +14,7 @@ const store= createStore(reducers, compose(applyMiddleware(thunk)))
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
- 
+  
        <App/>
   
 
