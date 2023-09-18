083081930edfef9037358eecdb69c2d7cba2dc8d
Brian Bandi <briankim7587@gmail.com>
2023-09-19 02:42:24 +0300

    login2

--- src/authentication/login/Login.jsx
+++ src/authentication/login/Login.jsx
@@ -23,7 +23,7 @@ const Login = () => {
       backgroundColor={theme.palette.background.alt}
       className="shadow-2xl">
       <Typography fontWeight="500" variant='h5' sx={{mb: "1.5rem"}}>
-        <p className='text-xl text-gray-600 font-semibold flex justify-center'>welcome to <span className="text-orange-600"> FastFood</span></p>
+        <p className='text-xl text-gray-600 font-semibold flex justify-center'>welcome to &#160;<span className="text-orange-600"> FastFood</span></p>
       </Typography>
       <LoginForm/>
       </Box>
